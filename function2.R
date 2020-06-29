cabb1fd7515282fce406d6deb20dc70e995c9ca2
divya-gurumoorthy <67547440+divya-gurumoorthy@users.noreply.github.com>
2020-06-29 11:42:46 +0530

    Delete function2.R

--- function2.R
+++
@@ -1,9 +0,0 @@
-complete <- function(directory, id=1:332) {
-  filelist <- list.files("G:\\Coursera R\\specdata",".csv", full.names = TRUE)
-  nobs <- numeric()
-  for (i in id) {
-    data <- read.csv(filelist[i])
-    nobs <- c(nobs, sum(complete.cases(data)))
-  }
-  data.frame(id, nobs)
-}
